bc3018b8e059b93c74bdedae592c3bbbac018ca7
conf8o <8confeito8@gmail.com>
2021-04-20 11:08:08 +0900

    Add double literal

--- Sources/swift-S-expression/Obj+Literal.swift
+++ Sources/swift-S-expression/Obj+Literal.swift
@@ -5,6 +5,13 @@ extension Obj: ExpressibleByIntegerLiteral {
     }
 }
 
+/// Objのリテラル表現(Double)
+extension Obj: ExpressibleByFloatLiteral {
+    public init(floatLiteral: Double) {
+        self = .double(floatLiteral)
+    }
+}
+
 /// Objのリテラル表現(String, Symbol)
 extension Obj: ExpressibleByStringLiteral {
     public init(stringLiteral: String) {
