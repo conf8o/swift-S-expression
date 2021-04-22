c060a5d679a2dcbaf2b1e51e77cb76dc2b8c0fbc
conf8o <8confeito8@gmail.com>
2021-04-22 11:57:20 +0900

    Fix order of declaring functions

--- Sources/swift-S-expression/main.swift
+++ Sources/swift-S-expression/main.swift
@@ -16,7 +16,6 @@ struct Main {
     mutating func run(_ objs: Obj...) -> Obj {
         run(objs)
     }
-
     mutating func run(_ objs: [Obj]) -> Obj {
         var last = Obj.null
         for obj in objs {
@@ -24,6 +23,54 @@ struct Main {
         }
         return last
     }
+}
+
+func lexicalAnalysisDebug() {
+    let s = """
+(+ 1 2)
+(+ 1.0 2.0)
+(/ 22.0 7.0)
+((lambda (x y) (+ x y)) 1 2)
+(define x (+ 1 2))
+(if (= 1 2) (+ 10 2) (% 5 2))
+(define f (lambda (x y) (* x y)))
+(f 10 2)
+(let ([x 1]
+      [y 10])
+    (+ x y))
+(define sum 10)
+((lambda (x) (+ x sum)) 100)
+(letrec
+    ([fact
+      (lambda (n)
+        (if (= 1 n)
+            1
+            (* n (fact (- n 1)))))])
+    (fact 5))
+(define fib
+    (lambda (n)
+        (if (= 0 n)
+            0
+            (if (= 1 n))
+                1
+                (+ (fib (- n 1))
+                   (fib (- n 2))))))
+(fib 9)
+((lambda (f x y) (f (+ x y) (* x y)))
+ * 10 5)
+
+(define (sum col)
+    (if (null? col)
+        0
+        (+ (car col) (sum (cdr col)))))
+(sum (list 1 2 3 4 5))
+(car (list "a" "b"))
+"""
+    let exprs = try! Obj.read(sExpr: s)
+    print(exprs)
+
+    var debug = Main(env: [[:]])
+    debug.debug(exprs)
 }
 
 func sExprDebug() {
@@ -79,52 +126,5 @@ func sExprDebug() {
     )
 }
 
-func lexicalAnalysisDebug() {
-    let s = """
-(+ 1 2)
-(+ 1.0 2.0)
-(/ 22.0 7.0)
-((lambda (x y) (+ x y)) 1 2)
-(define x (+ 1 2))
-(if (= 1 2) (+ 10 2) (% 5 2))
-(define f (lambda (x y) (* x y)))
-(f 10 2)
-(let ([x 1]
-      [y 10])
-    (+ x y))
-(define sum 10)
-((lambda (x) (+ x sum)) 100)
-(letrec
-    ([fact
-      (lambda (n)
-        (if (= 1 n)
-            1
-            (* n (fact (- n 1)))))])
-    (fact 5))
-(define fib
-    (lambda (n)
-        (if (= 0 n)
-            0
-            (if (= 1 n))
-                1
-                (+ (fib (- n 1))
-                   (fib (- n 2))))))
-(fib 9)
-((lambda (f x y) (f (+ x y) (* x y)))
- * 10 5)
-
-(define (sum col)
-    (if (null? col)
-        0
-        (+ (car col) (sum (cdr col)))))
-(sum (list 1 2 3 4 5))
-(car (list "a" "b"))
-"""
-    let tokens = try! Obj.read(sExpr: s)
-    print(tokens)
-
-    var debug = Main(env: [[:]])
-    debug.debug(tokens)
-}
 
 lexicalAnalysisDebug()
