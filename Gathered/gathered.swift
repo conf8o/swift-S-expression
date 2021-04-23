5e519d5477ebdcdc01a218acc44cc9dfa661bc15
conf8o <8confeito8@gmail.com>
2021-04-23 11:32:04 +0900

    Gathered

--- Gathered/gathered.swift
+++ Gathered/gathered.swift
@@ -233,16 +233,20 @@ extension Obj {
     static func tokenize(_ sExpr: String) -> [Token] {
         var tokens = [Token]()
         var textBuffer = ""
+        var isNotString = true
 
         for c in sExpr {
             switch c {
+            case "\"":
+                isNotString = !isNotString
+                textBuffer.append(c)
             case "(", "[":
                 tokens.absorbText(&textBuffer)
                 tokens.append(.pOpen)
             case ")", "]":
                 tokens.absorbText(&textBuffer)
                 tokens.append(.pClose)
-            case let c where c.isWhitespace || c.isNewline:
+            case let c where isNotString && (c.isWhitespace || c.isNewline):
                 tokens.absorbText(&textBuffer)
             default:
                 textBuffer.append(c)
@@ -250,6 +254,7 @@ extension Obj {
         }
         return tokens
     }
+
 
     static func read(sExpr: String) throws -> [Obj] {
         let tokens = tokenize(sExpr)
@@ -263,33 +268,23 @@ extension Obj {
                     throw LexicalAnalysisError.extraCloseParenthesis
                 }
                 stack[stack.count-1].append(Obj.S(p))
-            case .textBlock(let text):
-                // to int
+            case .textBlock(var text):
+                let last = stack.count - 1
+                
                 if let int = Int(text) {
-                    stack[stack.count-1].append(Obj.int(int))
-                }
-                // to double
-                else if let double = Double(text) {
-                    stack[stack.count-1].append(Obj.double(double))
-                }
-                // to string
-                else if let f = text.first, let l = text.last, f == "\"", l == "\"" {
-                    var string = text
-                    string.removeLast()
-                    string.removeFirst()
-                    stack[stack.count-1].append(Obj.string(string))
-                }
-                //to bool(true)
-                else if text == "#t" {
-                    stack[stack.count-1].append(Obj.bool(true))
-                }
-                // to bool(false)
-                else if text == "#f" {
-                    stack[stack.count-1].append(Obj.bool(false))
-                }
-                // to symbol
-                else {
-                    stack[stack.count-1].append(Obj.symbol("'" + text))
+                    stack[last].append(Obj.int(int))
+                } else if let double = Double(text) {
+                    stack[last].append(Obj.double(double))
+                } else if let f = text.first, let l = text.last, f == "\"", l == "\"" {
+                    text.removeLast()
+                    text.removeFirst()
+                    stack[last].append(Obj.string(text))
+                } else if text == "#t" {
+                    stack[last].append(Obj.bool(true))
+                } else if text == "#f" {
+                    stack[last].append(Obj.bool(false))
+                } else {
+                    stack[last].append(Obj.symbol("'" + text))
                 }
             }
         }
